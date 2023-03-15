4b8ab0a3c39f1fd24803158e4ad05fbe55b3568f
fallenAngel6502 <jwlynx26@gmail.com>
2023-03-14 23:08:03 -0400

    added cartridge file

---
+++ src/cartridge.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include <stdint.h>
+#include <string.h>
+#include "memory.h"
+
+//copy cartridge to memory
+void loadROM(unsigned char ROM[]) {
+    for (int i = 0; i < 0x4000; i++) {
+        write_mem(0x4020 , ROM[i+0x10]);
+    }
